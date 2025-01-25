2fe9dbdb817b348c9ce2a7f30ce100ca3009649a
someone1351 <someone1351@users.noreply.github.com>
2025-01-26 02:36:57 +1100

    some commit

--- src/interpreter/libs/math.rs
+++ src/interpreter/libs/math.rs
@@ -2,12 +2,12 @@ use std::collections::HashSet;
 
 use super::super::super::common::*;
 
-use super::super::func_context::*;
+// use super::super::func_context::*;
 use super::super::value::*;
-use super::super::error::*;
+// use super::super::error::*;
 use super::super::lib_scope::*;
 
-use super::super::data::*;
+// use super::super::data::*;
 
 #[derive(Clone)]
 pub struct Vec2([FloatT;2]);
@@ -96,6 +96,16 @@ fn set_field_util<T:Copy,const FROM_N: usize,const TO_N: usize>(fields:&str,from
 }
 
 pub fn register<X>(lib_scope : &mut LibScope<X>) {
+    //string(float)
+    lib_scope.method("string", |context|{
+        Ok(Value::string(format!("{:?}",context.param(0).as_float())))
+    }).float().end();
+    
+    //string(int)
+    lib_scope.method("string", |context|{
+        Ok(Value::string(format!("{:?}",context.param(0).as_int())))
+    }).int().end();
+
     //float(int)
     lib_scope.method("float", |context|{
         Ok(Value::float(context.param(0).as_float()))
@@ -269,7 +279,144 @@ pub fn register<X>(lib_scope : &mut LibScope<X>) {
     lib_scope.method("abs", |context|{
         Ok(Value::int(context.param(0).as_int().abs()))
     }).int().end();
+
+    //sign(float)
+    lib_scope.method("sign", |context|{
+        let a=context.param(0).as_float();
+        let b= if a.is_sign_positive() {1.0} else if a.is_sign_negative() {-1.0} else {0.0};
+        Ok(Value::float(b))
+    }).float().end();
+
+    //sign(int)
+    lib_scope.method("sign", |context|{
+        let a=context.param(0).as_int();
+        let b= if a > 0 {1} else if a < 0 {-1} else {0};
+        Ok(Value::int(b))
+    }).int().end();
+
+    //pow(float,float)
+    lib_scope.method("pow", |context|{
+        Ok(Value::float(context.param(0).as_float().powf(context.param(1).as_float())))
+    }).float().float().end();
+
+	//pow(int,int)
+    lib_scope.method("pow", |context|{
+        let a=context.param(0).as_int();
+        let b=context.param(1).as_int();
+        if b<0 {return Err(context.error("exp less than 0".to_string()));}
+        Ok(Value::int(a.pow(b as u32)))
+    }).int().int().end();
+
+	//mod(int,int)
+    lib_scope.method("mod", |context|{
+        let a=context.param(0).as_int();
+        let b=context.param(1).as_int();
+        Ok(Value::int(a%b))
+    }).int().int().end();
+
+    //floor(float)
+    lib_scope.method("floor", |context|{
+        Ok(Value::float(context.param(0).as_float().floor()))
+    }).float().end();
+
+    //ceil(float)
+    lib_scope.method("ceil", |context|{
+        Ok(Value::float(context.param(0).as_float().ceil()))
+    }).float().end();
+
+    //round(float)
+    lib_scope.method("round", |context|{
+        Ok(Value::float(context.param(0).as_float().round()))
+    }).float().end();
+
+    //trunc(float)
+    lib_scope.method("trunc", |context|{
+        Ok(Value::float(context.param(0).as_float().trunc()))
+    }).float().end();
+
+    //fract(float)
+    lib_scope.method("fract", |context|{
+        Ok(Value::float(context.param(0).as_float().fract()))
+    }).float().end();
     
+    //is_infinite(float)
+    lib_scope.method("is_infinite", |context|{
+        Ok(Value::Bool(context.param(0).as_float().is_infinite()))
+    }).float().end();
+
+    //is_nan(float)
+    lib_scope.method("is_nan", |context|{
+        Ok(Value::Bool(context.param(0).as_float().is_nan()))
+    }).float().end();
+    
+    //sqrt(float)
+    lib_scope.method("sqrt", |context|{
+        Ok(Value::float(context.param(0).as_float().sqrt()))
+    }).float().end();
+    
+    //cbrt(float)
+    lib_scope.method("cbrt", |context|{
+        Ok(Value::float(context.param(0).as_float().cbrt()))
+    }).float().end();
+
+    //cos(float)
+    lib_scope.method("cos",|context|{
+        Ok(Value::float(context.param(0).as_float().cos()))
+    }).float().end();
+    
+    //acos(float)
+    lib_scope.method("acos",|context|{
+        Ok(Value::float(context.param(0).as_float().acos()))
+    }).float().end();
+    
+    //sin(float)
+    lib_scope.method("sin",|context|{
+        Ok(Value::float(context.param(0).as_float().sin()))
+    }).float().end();
+
+    //asin(float)
+    lib_scope.method("asin",|context|{
+        Ok(Value::float(context.param(0).as_float().asin()))
+    }).float().end();
+
+    //tan(float)
+    lib_scope.method("tan",|context|{
+        Ok(Value::float(context.param(0).as_float().tan()))
+    }).float().end();
+    
+    //atan(float)
+    lib_scope.method("atan",|context|{
+        Ok(Value::float(context.param(0).as_float().atan()))
+    }).float().end();
+    
+    //atan2(float,float)
+    lib_scope.method("atan2",|context|{
+        Ok(Value::float(context.param(0).as_float().atan2(context.param(1).as_float())))
+    }).float().float().end();
+
+    //lerp(float,float,float)
+    lib_scope.method("lerp",|context|{
+        let x=context.param(0).as_float();
+        let y=context.param(1).as_float();
+        let a=context.param(2).as_float();
+        Ok(Value::float(x*(1.0-a)+y*a))
+    }).float().float().float().end();
+
+    //smooth_step(float,float,float)
+    lib_scope.method("smooth_step",|context|{
+        let edge0=context.param(0).as_float();
+        let edge1=context.param(1).as_float();
+        let x=context.param(2).as_float();
+        let t=((x-edge0)/(edge1-edge0)).clamp(0.0,1.0);
+        let q= t * t * (3.0 - 2.0 * t);
+        Ok(Value::float(q))
+    }).float().float().float().end();
+
+    //
+    lib_scope.insert_constant("pi", Value::float(std::f64::consts::PI));
+    lib_scope.insert_constant("pi2", Value::float(std::f64::consts::FRAC_PI_2));
+    lib_scope.insert_constant("pi4", Value::float(std::f64::consts::FRAC_PI_4));
+
     //vec2(ivec2)
     lib_scope.method("vec2",|context|{
         let v=context.param(0).as_custom().data_clone::<IVec2>()?;
--- src/interpreter/libs/mod.rs
+++ src/interpreter/libs/mod.rs
@@ -1,18 +1,10 @@
 pub mod array;
 pub mod dict;
 pub mod vararg;
-pub mod float;
-pub mod float_math;
-pub mod int;
-pub mod int_math;
-pub mod vector;
-pub mod matrix;
 pub mod string;
 pub mod utils;
 pub mod misc;
-pub mod vec;
-pub mod vec_math;
-pub mod ivec;
+pub mod math;
 
 use super::lib_scope::*;
 
@@ -23,16 +15,9 @@ pub fn register_all<X>(lib_scope : &mut LibScope<X>) {
     dict::register(lib_scope);
     vararg::register(lib_scope);
 
-    float::register(lib_scope);
-    float_math::register(lib_scope);
-    int::register(lib_scope);
-    int_math::register(lib_scope);
 
     string::register(lib_scope);
-    vector::register(lib_scope);
-    matrix::register(lib_scope);
 
-    vec::register(lib_scope);
-    vec_math::register(lib_scope);
+    math::register(lib_scope);
     
 }
