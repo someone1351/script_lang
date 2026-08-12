1e9e72ef87dfda27efc08eb26483b9669f62dfe0
someone1351 <someone1351@users.noreply.github.com>
2026-08-12 22:07:59 +1000

    some commit

--- src/langs/clike/grammar/mod.rs
+++ src/langs/clike/grammar/mod.rs
@@ -41,3 +41,39 @@ TODO
 
     so that if you  have "if i", it willsay expecting "block" instead of infix/field/call/index etc
 */
+
+/*
+NOTE
+    if the grammar is
+        S => V* Eol*
+        V => A (P B)?
+
+    if the input is
+        A Eol P
+
+    where Eol is considered whitespace and potentially ignored
+    then A is parsed, P is too, but fails for B, so it falls back to just parsing A
+        and then Eol as specified in S
+
+    P is cleared from expects, since S already parsed V* (ie A) and Eol*
+
+    A solution is to make items failing in an AND after the first to throw an error
+        so in V, P succeeding and B failing throws an error
+
+        but sometimes you don't want that eg
+            X => (A B) | (A C)
+
+        so the solution is to either modify the expects system to retain the failing B,
+            as it's token index is greater than the Eol's
+
+        or to have specialised ANDs
+            one that throws and error if any after first element fails
+            another like above with X, that stows (stores result) the first item automatically,
+                and either throw an error for any fail  after second element
+                or just not throw any errors at all
+            another that doesn't stow, and doesn't throw errors if an element fails (ie after first or second)
+
+        or just manually use Error nodes eg
+            V => A (P (B|Error))?
+
+*/
